8c579c0e32325433901e6e320f4bb38e8ff76069
jose-lam <jose.ruiz@utec.edu.pe>
2026-06-17 10:52:05 -0500

    include de archivos .h en expenses.h

--- include/expenses.h
+++ include/expenses.h
@@ -1,3 +1,8 @@
 #pragma once
 
 #include "expense_types.h"
+#include "expense_concepts.h"
+#include "expense_exporters.h"
+#include "expense_strategy.h"
+#include "expense_factory.h"
+#include "expense_decorators.h"
