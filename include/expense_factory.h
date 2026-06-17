02236c2cb2e32b6a3cdbccb1031c47d9bf2ddd83
jose-lam <jose.ruiz@utec.edu.pe>
2026-06-17 12:19:09 -0500

    correcion de bug en chrono que no inferia correctamente

--- include/expense_factory.h
+++ include/expense_factory.h
@@ -3,8 +3,8 @@
 #include "expense_concepts.h"
 
 template<ExpenseExporter E, class... Args>
-ExportFunction make_exporter(Args&&... args) {
-    return [](const ExpenseList& expenseList) {
-        return E::export_expenses(expenseList);
+auto make_exporter(Args&&... args) {
+    return [exporter = E{std::forward<Args>(args)...}](const ExpenseList& expenseList) {
+        return exporter.export_expenses(expenseList);
     };
 }
